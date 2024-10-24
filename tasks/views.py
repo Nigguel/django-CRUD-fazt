4bd617fd5836235aaee28f105f82169b95a6a59c
nigguel <nigguel14@gmail.com>
2024-10-23 22:21:31 -0300

    creando templates ycondicionales, login, logout y el modelo de tareas

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
@@ -39,3 +39,35 @@ def signup(request):
 
 def tasks(request):
     return render(request, "tasks.html")
+
+
+def create_task(request):
+    return render(request, "create_task.html")
+
+
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, "signin.html", {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is None:
+            return render(
+                request,
+                "signin.html",
+                {
+                    "form": AuthenticationForm,
+                    "error": "Username or password is incorrect",
+                },
+            )
+        else:
+            login(request, user)
+            return redirect("tasks")

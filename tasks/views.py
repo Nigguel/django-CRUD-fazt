1ec0722f7727badc78617ff087a4f5aaa3958765
nigguel <nigguel14@gmail.com>
2024-10-29 20:46:37 -0300

    estilizando las vistas con Bootstrap

--- tasks/views.py
+++ tasks/views.py
@@ -3,8 +3,9 @@ from .forms import TaskForm
 from django.utils import timezone
 from django.db import IntegrityError
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
@@ -40,11 +41,13 @@ def signup(request):
         )
 
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, "tasks.html", {"tasks": tasks})
 
 
+@login_required
 def tasks_completed(request):
     tasks = Task.objects.filter(
         user=request.user, datecompleted__isnull=False
@@ -52,6 +55,7 @@ def tasks_completed(request):
     return render(request, "tasks.html", {"tasks": tasks})
 
 
+@login_required
 def create_task(request):
 
     if request.method == "GET":
@@ -71,6 +75,7 @@ def create_task(request):
             )
 
 
+@login_required
 def task_detail(request, task_id):
     if request.method == "GET":
         task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -90,6 +95,7 @@ def task_detail(request, task_id):
             )
 
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == "POST":
@@ -98,6 +104,7 @@ def complete_task(request, task_id):
         return redirect("tasks")
 
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == "POST":
@@ -105,6 +112,7 @@ def delete_task(request, task_id):
         return redirect("tasks")
 
 
+@login_required
 def signout(request):
     logout(request)
     return redirect("home")
